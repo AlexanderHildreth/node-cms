a5ec1070a663592e6028cad86f99be3692a8ac88
Alexander Hildreth <zander.hildreth@gmail.com>
2020-03-11 16:00:14 +0100

    adding routes and changing links in views

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const bodyParser    = require('body-parser')
 const express       = require('express')
 const expHandlebars = require('express-handlebars')
+const mainRoutes    = require('./routes/home/home')
 const path          = require('path')
 
 const app   = express();
 const port  = process.env.PORT || 9999
 
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', mainRoutes)
 
+// Setting
 app.engine('handlebars', expHandlebars({ defaultLayout: 'home' }))
 app.set('view engine', 'handlebars')
-
-app.get('/', (req, res) => {
-    res.render('home/index')
-})
 
 app.listen(port, () =>{
     console.log(`Server is listening on port ${port}`)
