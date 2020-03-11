c29a32ca7238a27be51dddf7be676bd9d727adc4
Alexander Hildreth <zander.hildreth@gmail.com>
2020-03-11 16:53:38 +0100

    Updated files for admin and home views incl routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const bodyParser    = require('body-parser')
 const express       = require('express')
 const expHandlebars = require('express-handlebars')
-const mainRoutes    = require('./routes/home/home')
+const homeRoutes    = require('./routes/home/home')
+const adminRoutes   = require('./routes/admin/admin')
 const path          = require('path')
 
 const app   = express();
@@ -9,12 +10,13 @@ const port  = process.env.PORT || 9999
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', mainRoutes)
+app.use('/', homeRoutes)
+app.use('/admin', adminRoutes)
 
 // Setting
 app.engine('handlebars', expHandlebars({ defaultLayout: 'home' }))
 app.set('view engine', 'handlebars')
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
