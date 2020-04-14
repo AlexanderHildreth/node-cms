fb23b3ecdcc498bf32bb79e018c8a00792a623e1
Alexander Hildreth <zander.hildreth@gmail.com>
2020-04-14 19:23:59 +0200

    change test dev url

--- config/devDatabase.js
+++ config/devDatabase.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb+srv://node-cms-dev-user-root-01:pxJDq7fbbXNoCIuc@node-cms-dev-01-g1qem.mongodb.net/test?retryWrites=true&w=majority'
+    url: 'mongodb://node-cms-dev-user-root-01:pxJDq7fbbXNoCIuc@ds049130.mlab.com:49130/heroku_d7pwhjnf'
 };
