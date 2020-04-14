6f9054cc176962af7c5e43d96b45331bd45b179c
Alexander Hildreth <zander.hildreth@gmail.com>
2020-04-14 21:24:03 +0200

    up to date app

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 // Modules
-const bodyParser        = require('body-parser')
-const express           = require('express')
-const expHandlebars     = require('express-handlebars')
-const flash             = require('connect-flash')
-const methodOverride    = require('method-override')
-const mongoose          = require('mongoose')
-const path              = require('path')
-const session           = require('express-session')
-const upload            = require('express-fileupload')
+const bodyParser = require('body-parser')
+const express = require('express')
+const expHandlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const path = require('path')
+const session = require('express-session')
+const upload = require('express-fileupload')
 // Models
-const postsModel        = require('./models/Post')
+const postsModel = require('./models/Post')
 // Routes
-const adminRoutes       = require('./routes/admin/admin')
-const categoryRoutes    = require('./routes/admin/categories')
-const homeRoutes        = require('./routes/home/home')
-const postRoutes        = require('./routes/admin/posts')
+const adminRoutes = require('./routes/admin/admin')
+const categoryRoutes = require('./routes/admin/categories')
+const homeRoutes = require('./routes/home/home')
+const postRoutes = require('./routes/admin/posts')
 // const vars
-const app               = express();
-const mongoDbUrl        = require('./config/database');
-// const mongoDbUrl        = require('dotenv').config({ path: './config/database' });
-const port              = process.env.PORT || 9999
+const app = express();
+const appPort = process.env.PORT || 9999
 const handlebarsHelpers = require('./helpers/handlebarsHelpers')
+const mongoDbUrl = require('./config/database');
+const mongoDbPort = process.env.MONGOD_DB_PORT || 27017;
 
 // DB connection
 mongoose.Promise = global.Promise
 
 mongoose.connect(mongoDbUrl.url, { useNewUrlParser: true, useUnifiedTopology: true }).then((db) => {
-    console.log('DB connection established, listening on port 27017...')
+    console.log(`DB connection established, to db: ${mongoDbPort}...`)
 }).catch((err) => {
     console.log(`There was an error establishing connection:\n${err}`)
-    console.log(`\n${mongoDbURL}`) 
+    console.log(`\n${mongoDbURL}`)
 })
 
 // Middleware
@@ -57,10 +57,10 @@ app.use('/admin/categories', categoryRoutes)
 app.use('/admin/posts', postRoutes)
 
 // Setting
-app.engine('handlebars', expHandlebars({ defaultLayout: 'home', helpers: handlebarsHelpers}))
+app.engine('handlebars', expHandlebars({ defaultLayout: 'home', helpers: handlebarsHelpers }))
 app.set('view engine', 'handlebars')
 
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
+app.listen(appPort, () => {
+    console.log(`Server is listening on port ${appPort}...`)
 })
