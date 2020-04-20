2c057a7c3af21e3783c0ccc3b10a2002df5d7cce
Alexander Hildreth <zander.hildreth@gmail.com>
2020-04-20 14:21:37 +0200

    improvments

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const app       = require('./app');
-const dotenv    = require('dotenv');
+const server    = require('./app');
 const appPort   = process.env.PORT || 9999
 require('./config/database');
 
-dotenv.config({ path: './secrets.env' });
-
-app.listen(appPort, () => {
+const app = server.listen(appPort, () => {
     console.log(`Server is listening on port ${appPort}...`)
 })
+
+module.exports = app;
